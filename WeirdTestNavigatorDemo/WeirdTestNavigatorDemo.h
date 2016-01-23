5258ce4290850a2846b6ea6e1ef96b62b9abe89d
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-01-23 20:35:22 +0100

    Initial Commit

---
+++ WeirdTestNavigatorDemo/WeirdTestNavigatorDemo.h
@@ -0,0 +1,19 @@
+//
+//  WeirdTestNavigatorDemo.h
+//  WeirdTestNavigatorDemo
+//
+//  Created by Cihat Gündüz on 23.01.16.
+//  Copyright © 2016 Open Source. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WeirdTestNavigatorDemo.
+FOUNDATION_EXPORT double WeirdTestNavigatorDemoVersionNumber;
+
+//! Project version string for WeirdTestNavigatorDemo.
+FOUNDATION_EXPORT const unsigned char WeirdTestNavigatorDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeirdTestNavigatorDemo/PublicHeader.h>
+
+
